2101335a723b127099916326a6ee9d6c3b2dd851
Mikulash <heinz.mikulas@gmail.com>
2021-08-14 00:55:19 +0200

    setup

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+function show(relevant, others) {
+  let prvek = document.getElementById(relevant);
+  prvek.style.display = "block";
+  others.forEach(id => hide(id));
+}
+
+function hide(id){
+  let prvek = document.getElementById(id);
+  prvek.style.display = "none";
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+  let birth = new Date("12/15/1998");
+  let today = new Date();
+  let seconds = today.getTime() - birth.getTime();
+  let days = seconds / (24*60*60*1000);
+  let prvek = document.getElementById("time");
+  prvek.textContent = Math.round(days);
+})
